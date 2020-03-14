341804e1751a0bf8e4e0ac5d261748e940338805
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-14 13:43:15 +0000

    Add search functionality to the search component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,12 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import * as api from "../api";
 import ImageCard from "./ImageCard";
 
 export default function Search() {
+  const ref = useRef(false);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [imageData, setImageData] = useState([]);
+  const [textInput, setTextInput] = useState("");
+  const [searchTerm, setSearchTerm] = useState();
+
+  useEffect(() => {
+    if (ref.current) {
+      api.getImageData(searchTerm).then(res => {
+        setImageData(res);
+      });
+    } else {
+      ref.current = true;
+    }
+  }, [searchTerm]);
+
   const images = [];
   for (let i = 0; i < 9; i++) {
     images.push(<ImageCard title={i} />);
   }
+
+  const handleChange = event => {
+    setTextInput(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    setSearchTerm(textInput);
+    event.preventDefault();
+  };
 
   return (
     <div className="searchContainer">
@@ -14,10 +39,15 @@ export default function Search() {
         <form
           onSubmit={event => {
             console.log("submitted");
-            event.preventDefault();
+            handleSubmit(event);
           }}
         >
-          <input type="text" />
+          <input
+            type="text"
+            onChange={event => {
+              handleChange(event);
+            }}
+          />
           <input type="submit" value="Submit" />
         </form>
       </div>
