203ec27f856d894d488ab839bc97648c89ce9bb3
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-14 16:52:06 +0000

    Added SetLoading to onClick Events

--- src/Components/PageChooser.js
+++ src/Components/PageChooser.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default function PageChooser(props) {
-  const { pageNumber, setPageNumber } = props;
+  const { pageNumber, setPageNumber, setLoading } = props;
 
   return (
     <div className="pageChooser">
       <button
         onClick={() => {
+          setLoading(true)
           setPageNumber(prevPageNumber => {
             return prevPageNumber - 1;
           });
@@ -17,6 +18,7 @@ export default function PageChooser(props) {
       <h3>Page: {pageNumber + 1}</h3>
       <button
         onClick={() => {
+          setLoading(true)
           setPageNumber(prevPageNumber => {
             return prevPageNumber + 1;
           });
