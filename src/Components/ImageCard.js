4b3c7a9efe4ff4a662cd319fb82e3d949721203c
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-14 11:12:26 +0000

    Add in basic structure of ImageCard

---
+++ src/Components/ImageCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ImageCard(props) {
+  const { title } = props;
+
+  return (
+    <div className="imageCard">
+      <img
+        src="https://i.stack.imgur.com/yZlqh.png"
+        alt="box"
+        className="nasaImage"
+      />
+      <h2 className="nasaImage">{title + 1}</h2>
+    </div>
+  );
+}
