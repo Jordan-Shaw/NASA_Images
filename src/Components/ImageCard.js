d5268187ea0989ff721bdb3219ea69d7865b316e
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-14 15:48:51 +0000

    Add getImageLink functionality and link to image rendering

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import * as api from "../api";
 
 export default function ImageCard(props) {
   const { title, imageData } = props;
-  console.log(imageData);
+  const [nasa_id, setNasa_id] = useState(imageData.data[0].nasa_id);
+  const [href, setHref] = useState("");
+  console.log(12345, href);
+
+  useEffect(() => {
+    console.log("request-sent");
+    api.getImageLink(nasa_id).then(res => {
+      const { href } = res;
+      setHref(href);
+    });
+  }, []);
 
   return (
     <div className="imageCard">
-      <img
-        src="https://i.stack.imgur.com/yZlqh.png"
-        alt="box"
-        className="nasaImage"
-      />
+      <img src={href} alt="box" className="nasaImage" />
       <h2 className="nasaImage">{title + 1}</h2>
     </div>
   );
