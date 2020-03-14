199e2c5e407fc24bf5391c6a21428f05d0586f0c
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-14 10:50:24 +0000

    Add the basic structure of the Search component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import * as api from "../api";
+import ImageList from "./ImageList";
 
-export default class Search extends Component {
-  state = {
-    images: []
-  }
-
-  render() {
-    return (
-      <div>
-        <input type="text"/>
-        <input type="submit" value="Submit"/>
-      </div>
-    )
-  }
+export default function Search() {
+  return (
+    <div>
+      <form
+        onSubmit={event => {
+          console.log("submitted");
+          event.preventDefault();
+        }}
+      >
+        <input type="text" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
