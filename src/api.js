533f52880d2e0f0981f3bb29f6e3152a3805b276
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-14 13:42:32 +0000

    Create getImageData function

--- src/api.js
+++ src/api.js
@@ -1,3 +1,17 @@
 import axios from "axios";
 
 const apiRoot = "https://images-api.nasa.gov";
+
+export const getImageData = searchTerm => {
+  return axios
+    .get(`${apiRoot}/search`, {
+      params: {
+        q: `${searchTerm}`,
+        media_type: "image"
+      }
+    })
+    .then(res => {
+      console.log(1, res);
+      return res.data.collection.items;
+    });
+};
