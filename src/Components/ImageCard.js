6f2ba7827253b38580edd35c552fa3a4f3af7b67
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-18 11:53:57 +0000

    Simplified imageCard by removing unnecessary request

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -2,21 +2,22 @@ import React, { useEffect, useState } from "react";
 import * as api from "../api";
 
 export default function ImageCard(props) {
-  const { title, imageData } = props;
-  let nasa_id = imageData.data[0].nasa_id;
-  const [href, setHref] = useState("");
+  // const { title } = props;
+  const { imageData } = props;
+  // let nasa_id = imageData.data[0].nasa_id;
+  const {href} = imageData.links[0]
+  // const [href, setHref] = useState("");
 
-  useEffect(() => {
-    api.getImageLink(nasa_id).then(res => {
-      const { href } = res;
-      setHref(href);
-    });
-  });
+  // useEffect(() => {
+  //   api.getImageLink(nasa_id).then(res => {
+  //     const { href } = res;
+  //     setHref(href);
+  //   });
+  // });
 
   return (
     <div className="imageCard">
       <img src={href} alt="box" className="nasaImage" />
-      <h2 className="nasaImage">{title + 1}</h2>
     </div>
   );
 }
