fd927e1591cbd010ff8570a68d165cadce7ccd41
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-18 21:17:12 +0000

    add the getSingleImage function to the api file

--- src/api.js
+++ src/api.js
@@ -16,6 +16,23 @@ export const getImageData = searchTerm => {
     });
 };
 
+export const getSingleImageData = nasa_id => {
+  return axios
+    .get(`${apiRoot}/search`, {
+      params: {
+        nasa_id: `${nasa_id}`,
+        media_type: "image"
+      }
+    })
+    .then(res => {
+      const { items } = res.data.collection;
+      return items;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
 export const getImageLink = nasa_id => {
   return axios.get(`${apiRoot}/asset/${nasa_id}`).then(res => {
     return res.data.collection.items[0];
