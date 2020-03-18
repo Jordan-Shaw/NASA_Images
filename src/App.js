f671670a442e103ff832852a6d6c3f07bdc53720
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-18 21:19:50 +0000

    Add the router to the app so singleImage pages can be reached

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header';
-import Search from './Components/Search';
+import React from "react";
+import { Router } from "@reach/router";
+import "./App.css";
+import Main from "./Pages/Main";
+import SingleImage from "./Pages/SingleImage";
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Search />
+      <Router>
+        <Main path="/"/>
+        <SingleImage path="/:nasa_id"/>
+      </Router>
     </div>
   );
 }
